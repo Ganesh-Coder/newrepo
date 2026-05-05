0bba9936622a69603842711139c7226439f4f0f2
Ganesh-Coder <ganesh.shingare1@gmail.com>
2026-05-05 22:35:43 +0530

    backend changes

--- index.java
+++ index.java
@@ -4,7 +4,7 @@
 <?java    ?>
 public class index {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(" this is backend code ");
     }
 }
 
