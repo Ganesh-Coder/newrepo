f002ca09235f9e3c3b55977f930ec1b46c7dd9b9
Ganesh-Coder <ganesh.shingare1@gmail.com>
2026-05-06 20:04:33 +0530

    new changes

--- index.java
+++ index.java
@@ -10,6 +10,7 @@ public class index {
         System.out.println("Hello, World!");
 
         system.out.println("This is main branch commit.");
+        system.out.println("This is main branch new changes.");
     }
 }
 
